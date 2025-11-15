d06acefe397bcf30b74ac9ec360293d64001d973
nawfaldo <nawfaldo.fazli@gmail.com>
2025-11-15 15:39:41 +0700

    idk

---
+++ api.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import pandas as pd
+
+model_dict = joblib.load("model.pkl")
+pipeline = model_dict["pipeline"]
+threshold = model_dict["threshold"]
+
+app = FastAPI()
+
+class MachineInput(BaseModel):
+    UDI: int
+    Product_ID: str
+    Type: str
+    Air_temperature_K: float
+    Process_temperature_K: float
+    Rotational_speed_rpm: int
+    Torque_Nm: float
+    Tool_wear_min: int
+    Target: int
+    Failure_Type: str
+
+@app.post("/predict")
+def predict_api(data: MachineInput):
+    df = pd.DataFrame([data.dict()])
+
+    df["quality"] = df["Product_ID"].str[0]
+    df["serial"] = df["Product_ID"].str[1:].astype(int)
+
+    df["temp_diff"] = df["Process_temperature_K"] - df["Air_temperature_K"]
+    df["load"] = df["Torque_Nm"] * df["Rotational_speed_rpm"]
+    df["stress"] = df["load"] / (df["Process_temperature_K"] + 1e-9)
+    df["wear_rate"] = df["Tool_wear_min"] / (df["serial"] + 1e-9)
+    df["torque_temp_inter"] = df["Torque_Nm"] * df["Process_temperature_K"]
+    df["high_rpm"] = (df["Rotational_speed_rpm"] > df["Rotational_speed_rpm"].median()).astype(int)
+
+    df["quality_ord"] = df["quality"].map({'L': 0, 'M': 1, 'H': 2})
+
+    if "Target" in df.columns:
+        df = df.drop(columns=["Target"])
+    if "Failure_Type" in df.columns:
+        df = df.drop(columns=["Failure_Type"])
+
+    prob = pipeline.predict_proba(df)[0][1]
+
+    pred = 1 if prob >= threshold else 0
+
+    return {
+        "probability": float(prob),
+        "threshold": float(threshold),
+        "prediction": "FAILURE" if pred == 1 else "NO FAILURE"
+    }
---
+++ inference.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import requests
+import json
+
+API_URL = "http://127.0.0.1:8000/predict"
+INPUT_CSV = "inference.csv"
+OUTPUT_CSV = "result_inference.csv"
+
+def main():
+    df = pd.read_csv(INPUT_CSV)
+
+    for index, row_data in df.iterrows():
+        payload = row_data.to_dict()
+
+        try:
+            response = requests.post(API_URL, json=payload)
+            response.raise_for_status()
+
+            api_result = response.json()
+
+            print(f"Row {index} OK → Prediction: {api_result['prediction']}")
+        except requests.exceptions.RequestException as e:
+            print(f"Gagal mengirim data row {index}: {e}")
+
+if __name__ == "__main__":
+    main()
